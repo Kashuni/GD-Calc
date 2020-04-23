8756b7c001952c51dce4004541fa18f5798fda7b
hpmmadushanka <62623167+hpmmadushanka@users.noreply.github.com>
2020-04-23 13:17:49 +0530

    Add files via upload

---
+++ GDC/application.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboardSample.html")
+
+@app.route("/login")
+def login():
+    return render_template("index.html")
